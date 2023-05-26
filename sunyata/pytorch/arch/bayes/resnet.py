c6bd97aa8132119fd5d6eb1b05509c710f0314b6
SGuoying <826367045@qq.com>
2023-05-26 18:30:30 +0800

    update

--- sunyata/pytorch/arch/bayes/resnet.py
+++ sunyata/pytorch/arch/bayes/resnet.py
@@ -7,6 +7,8 @@ from torch.nn import Parameter
 import torch.nn.functional as F
 from torchvision.models.resnet import ResNet, BasicBlock, Bottleneck
 
+from sunyata.pytorch.arch.base import SE
+
 # %%
 class BayesResNet(ResNet):
     def __init__(
@@ -94,27 +96,27 @@ class BayesResNet2(ResNet):
         expansion = block.expansion
         self.digups = nn.ModuleList([
             *[nn.Sequential(
+                nn.Conv2d(64 * i * expansion, 2048, kernel_size=1),
                 nn.AdaptiveAvgPool2d((1, 1)),
                 nn.Flatten(),
-                nn.Linear(64 * i * expansion, num_classes)
                 ) for i in (1, 2, 4) 
                 ],
             nn.Sequential(
                 self.avgpool,
                 nn.Flatten(),
-                self.fc,
+                # self.fc,
             )
         ])
 
-        log_prior = torch.zeros(1, num_classes)
+        log_prior = torch.zeros(1, 2048)
         self.register_buffer('log_prior', log_prior)
-        self.logits_layer_norm = nn.LayerNorm(num_classes)
+        self.logits_layer_norm = nn.LayerNorm(2048)
         # self.logits_bias = Parameter(torch.zeros(1, num_classes), requires_grad=True)
 
     def _forward_impl(self, x: Tensor) -> Tensor:
         batch_size, _, _, _ = x.shape
         log_prior = self.log_prior.repeat(batch_size, 1)
-        log_priors = torch.empty(0)
+        # log_priors = torch.empty(0)
 
         x = self.conv1(x)
         x = self.bn1(x)
@@ -127,17 +129,17 @@ class BayesResNet2(ResNet):
         ]):
             for block in layer:
                 x = block(x)
-                log_prior = self.digups[i](x)
-                # log_prior = log_prior + logits
-                # log_prior = self.logits_layer_norm(log_prior)
-                log_priors = torch.cat([log_priors, log_prior])
+                logits = self.digups[i](x)
+                log_prior = log_prior + logits
+                log_prior = self.logits_layer_norm(log_prior)
+                # log_priors = torch.cat([log_priors, log_prior])
                 # log_prior = log_prior - torch.mean(log_prior, dim=-1, keepdim=True) + self.logits_bias
                 # log_prior = F.log_softmax(log_prior, dim=-1)
-        return log_priors
+        return self.fc(log_prior)
 
 # %%
 class ResNet2(ResNet):
-    def __init__(
+   def __init__(
         self,
         block: Type[Union[BasicBlock, Bottleneck]],
         layers: List[int],
@@ -159,23 +161,87 @@ class ResNet2(ResNet):
             norm_layer,
         )
 
+        expansion = block.expansion
+        self.digups = nn.ModuleList([
+            *[nn.Sequential(
+                nn.Conv2d(64 * i * expansion, 2048, kernel_size=1),
+                nn.AdaptiveAvgPool2d((1, 1)),
+                nn.Flatten(),
+                ) for i in (1, 2, 4) 
+                ],
+            nn.Sequential(
+                self.avgpool,
+                # nn.Flatten(),
+            )
+        ])
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(512 * block.expansion, num_classes)
+            )
 
-    def _forward_impl(self, x: Tensor) -> Tensor:
-        # See note [TorchScript super()]
+        log_prior = torch.zeros(1, 2048)
+        self.register_buffer('log_prior', log_prior)
+        # self.logits_layer_norm = nn.LayerNorm(2048)
+        self.logits_layer_norm = SE(2048)
+        # self.logits_bias = Parameter(torch.zeros(1, num_classes), requires_grad=True)
+
+   def _forward_impl(self, x: Tensor) -> Tensor:
+        batch_size, _, _, _ = x.shape
+        log_prior = self.log_prior.repeat(batch_size, 1)
+        # log_priors = torch.empty(0)
+
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
         x = self.maxpool(x)
 
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        # x = self.layer4(x)
-        multi_logits = torch.empty(0)
-        for block in self.layer4:
-            x = block(x)
-            logits = self.avgpool(x)
-            logits = torch.flatten(logits, 1)
-            logits = self.fc(logits)
-            multi_logits = torch.cat([multi_logits, logits])
-        return multi_logits
+        for i, layer in enumerate([
+            self.layer1, self.layer2,
+            self.layer3, self.layer4
+        ]):
+            for block in layer:
+                x = block(x)
+                logits = self.digups[i](x)
+                log_prior = log_prior + logits
+                log_prior = self.logits_layer_norm(log_prior)
+        return self.fc(log_prior)
+
+
+def _resnet(
+    block: Type[Union[BasicBlock, Bottleneck]],
+    layers: List[int],
+    **kwargs: Any,
+) -> ResNet:
+
+    model = ResNet2(block, layers, **kwargs)
+    return model
+
+from torchvision.models._api import register_model
+@register_model()
+def resnet50(**kwargs: Any) -> ResNet:
+    """ResNet-50 from `Deep Residual Learning for Image Recognition <https://arxiv.org/pdf/1512.03385.pdf>`__.
+
+    .. note::
+       The bottleneck of TorchVision places the stride for downsampling to the second 3x3
+       convolution while the original paper places it to the first 1x1 convolution.
+       This variant improves the accuracy and is known as `ResNet V1.5
+       <https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch>`_.
+
+    Args:
+        weights (:class:`~torchvision.models.ResNet50_Weights`, optional): The
+            pretrained weights to use. See
+            :class:`~torchvision.models.ResNet50_Weights` below for
+            more details, and possible values. By default, no pre-trained
+            weights are used.
+        progress (bool, optional): If True, displays a progress bar of the
+            download to stderr. Default is True.
+        **kwargs: parameters passed to the ``torchvision.models.resnet.ResNet``
+            base class. Please refer to the `source code
+            <https://github.com/pytorch/vision/blob/main/torchvision/models/resnet.py>`_
+            for more details about this class.
+
+    .. autoclass:: torchvision.models.ResNet50_Weights
+        :members:
+    """
+
+    return _resnet(Bottleneck, [3, 4, 6, 3], **kwargs)

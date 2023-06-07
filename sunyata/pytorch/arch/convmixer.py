539390bb224fc5d446ee99e7ad6400fb92e7cabd
SGuoying <826367045@qq.com>
2023-06-07 19:46:03 +0800

    256

--- sunyata/pytorch/arch/convmixer.py
+++ sunyata/pytorch/arch/convmixer.py
@@ -297,7 +297,7 @@ class ConvMixerCat(nn.Module):
             nn.Flatten(),
             # nn.Linear(cfg.hidden_dim, cfg.num_classes)
         )
-        self.attn = Attention(cfg.hidden_dim*cfg.num_layers)
+        self.attn = Attention(cfg.hidden_dim)
             
         self.layer_norm = nn.LayerNorm(cfg.hidden_dim)
         self.fc = nn.Sequential(

6c71568c13c738d66f0c253b0793e789942bdce3
SGuoying <826367045@qq.com>
2023-06-13 19:07:57 +0800

    update

--- sunyata/mosaicml/model_v3.py
+++ sunyata/mosaicml/model_v3.py
@@ -161,8 +161,8 @@ def deeplabv3(num_classes: int,
     head = torchvision.models.segmentation.deeplabv3.DeepLabHead(2048,
                                                                   num_classes=num_classes)
 
-    # model = SimpleSegmentationModel(backbone, head)
-    model = DeepLabV3(backbone, head)
+    model = SimpleSegmentationModel(backbone, head)
+    # model = DeepLabV3(backbone, head)
 
     # Only apply initialization to classifier head if pre-trained weights are used
     if init_fn:

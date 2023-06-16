e4e5af9a6830d92306ff85cc2d3cef14d8aecee8
SGuoying <826367045@qq.com>
2023-06-16 16:28:22 +0800

    update

--- sunyata/mosaicml/model.py
+++ sunyata/mosaicml/model.py
@@ -5,7 +5,7 @@ from composer.metrics import CrossEntropy
 from composer.models import ComposerClassifier
 from torchmetrics import MetricCollection
 from torchmetrics.classification import MulticlassAccuracy
-from sunyata.pytorch.arch.conformer import Conformer
+from sunyata.pytorch.arch.conformer import Conformer, Conformer2
 
 from sunyata.pytorch.arch.convmixer import BayesConvMixer3, BayesConvMixer4, BayesConvMixer5, ConvMixer2, ConvMixerCat, ConvMixerCfg, ConvMixer, BayesConvMixer, ConvMixereca
 from sunyata.pytorch.arch.iterative import ConvMixerattn2, ConvMixerattn3
@@ -46,6 +46,8 @@ def build_composer_convmixer(model_name: str = 'convmixer',
         model = BayesConvMixer5(cfg)
     elif model_name == "Conformer":
         model = Conformer(cfg)
+    elif model_name == "Conformer2":
+        model = Conformer2(cfg)
 
     else:
         raise ValueError(f"model_name='{model_name}' but only 'convmixer' and 'bayes_convmixer' are supported now.")
--- sunyata/pytorch/arch/conformer.py
+++ sunyata/pytorch/arch/conformer.py
@@ -4,7 +4,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from sunyata.pytorch.arch.base import BaseCfg
+from sunyata.pytorch.arch.base import BaseCfg, Residual
+from sunyata.pytorch.arch.conformer import ConvMixerCfg
 
 
 
@@ -116,8 +117,84 @@ class Conformer(nn.Module):
 
         latent = reduce(latent, 'b n d -> b d', 'mean')
         return self.to_logits(latent)
-           
-        
+
+class Mlp(nn.Module):
+    def __init__(self, dim, hidden_dim=None, dropout = 0.):
+        super().__init__()
+        hidden_dim = dim if hidden_dim is None else hidden_dim
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
+    
+class ConvLayer2(nn.Sequential):
+    def __init__(self, hidden_dim, kernel_size, bias=False):
+        super().__init__(
+            nn.Conv2d(hidden_dim, hidden_dim, 1),
+            nn.BatchNorm2d(hidden_dim),
+            nn.GELU(),
+            nn.Conv2d(hidden_dim, hidden_dim, kernel_size, padding=kernel_size // 2, bias=bias),
+            nn.BatchNorm2d(hidden_dim),
+            nn.GELU(),
+        )     
+
+class Conformer2(Conformer):
+    def __init__(self, cfg: ConvMixerCfg):
+        super().__init__(cfg)
+        self.embed = nn.Sequential(
+            nn.Conv2d(3, cfg.hidden_dim, cfg.patch_size, stride=cfg.patch_size),
+            nn.BatchNorm2d(cfg.hidden_dim),
+            nn.GELU(),
+        )
+
+        self.layers = nn.ModuleList([
+            ConvLayer2(cfg.hidden_dim, cfg.kernel_size)
+            for _ in range(cfg.num_layers)
+        ])
+        self.conv1 = nn.Conv2d(cfg.hidden_dim, cfg.hidden_dim, 1)
+        self.mlp = Mlp(cfg.hidden_dim, cfg.hidden_dim * 4)
+
+        self.attn_layers = AttnLayer(query_dim=cfg.hidden_dim,
+                                    context_dim=cfg.hidden_dim,
+                                    heads=1,
+                                    dim_head=cfg.hidden_dim,
+                                    )
+        self.norm = nn.LayerNorm(cfg.hidden_dim)
+        self.to_logits = nn.Linear(cfg.hidden_dim, cfg.num_classes)
+        self.latent = nn.Parameter(torch.randn(1, cfg.hidden_dim))
+
+    def forward(self, x):
+        batch_size, _, _, _ = x.shape
+        latent = repeat(self.latent, 'n d -> b n d', b=batch_size)
+
+        x = self.embed(x)
+        x = self.conv1(x)
+        input = x.permute(0, 2, 3, 1)
+        input = rearrange(input, 'b ... d -> b (...) d')
+        latent = torch.cat([latent[:, 0][:, None, :], input], dim=1)
+        latent = latent + self.attn_layers(latent, input)
+        mlp = self.mlp(latent)
+
+        for layer in self.layers:
+            x = x + layer(x)
+            x = self.conv1(x)
+            input = x.permute(0, 2, 3, 1)
+            input = rearrange(input, 'b ... d -> b (...) d')
+            # latent = torch.cat([latent[:, 0][:, None, :], input], dim=1)
+            latent = latent + self.attn_layers(latent, input)
+            latent = self.norm(latent)
+            mlp = self.mlp(latent) + mlp
+
+        latent = reduce(mlp, 'b n d -> b d', 'mean')
+        return self.to_logits(latent)
+
+
+
 
 
         

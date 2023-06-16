36712ab0aa55a022ef259609ff434f519a4940d6
SGuoying <826367045@qq.com>
2023-06-16 21:37:45 +0800

    update

--- sunyata/pytorch/arch/conformer.py
+++ sunyata/pytorch/arch/conformer.py
@@ -38,33 +38,53 @@ class ConvLayer(nn.Sequential):
             nn.GELU(),
         )
 
-class Convolution(nn.Module):
-    def __init__(self,
-                 cfg: ConvMixerCfg):
+class Mlp(nn.Module):
+    def __init__(self, dim, hidden_dim=None, dropout = 0.):
         super().__init__()
-        self.cfg = cfg
-        self.layers = nn.ModuleList([
-            ConvLayer(cfg.hidden_dim, cfg.kernel_size)
-            for _ in range(cfg.num_layers)
-        ])
-        self.embed = nn.Sequential(
-            nn.Conv2d(3, cfg.hidden_dim, cfg.patch_size, stride=cfg.patch_size),
-            nn.BatchNorm2d(cfg.hidden_dim),
+        hidden_dim = dim if hidden_dim is None else hidden_dim
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
             nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
         )
-        self.digup = nn.Sequential(
-            nn.AdaptiveAvgPool2d((1, 1)),
-            nn.Flatten(),
-            nn.Linear(cfg.hidden_dim, cfg.num_classes)
-        )
-
     def forward(self, x):
-        x = self.embed(x)
-        for layer in self.layers:
-            x = x + layer(x)
-        x = self.digup(x)
-        return x
+        return self.net(x)
+    
+class ConvLayer2(nn.Sequential):
+    def __init__(self, hidden_dim, kernel_size, bias=False):
+        super().__init__(
+            nn.Conv2d(hidden_dim, hidden_dim, 1),
+            nn.BatchNorm2d(hidden_dim),
+            nn.GELU(),
+            nn.Conv2d(hidden_dim, hidden_dim, kernel_size, padding=kernel_size // 2, bias=bias),
+            nn.BatchNorm2d(hidden_dim),
+            nn.GELU(),  
+        ) 
 
+class ConvLayer3(nn.Sequential):
+    def __init__(self, hidden_dim, kernel_size, bias=False):
+        super().__init__(
+            Residual(nn.Sequential(nn.Conv2d(hidden_dim, hidden_dim, 1),
+            nn.BatchNorm2d(hidden_dim),
+            nn.GELU(),
+            nn.Conv2d(hidden_dim, hidden_dim, kernel_size, padding=kernel_size // 2, bias=bias),
+            nn.BatchNorm2d(hidden_dim),
+            nn.GELU(),
+            )),
+            nn.Conv2d(hidden_dim, hidden_dim, 1),
+            nn.BatchNorm2d(hidden_dim),
+            nn.GELU(),
+        )    
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
 
 class AttnLayer(nn.Module):
     def __init__(self, 
@@ -95,7 +115,39 @@ class AttnLayer(nn.Module):
         out = torch.einsum('b i j, b j d -> b i d', attn, v)
         out = rearrange(out, 'b (h n) d -> b n (h d)', h=h)
         return self.to_out(out)
-    
+
+
+### layer 0 #######################################
+class Convolution(nn.Module):
+    def __init__(self,
+                 cfg: ConvMixerCfg):
+        super().__init__()
+        self.cfg = cfg
+        self.layers = nn.ModuleList([
+            ConvLayer(cfg.hidden_dim, cfg.kernel_size)
+            for _ in range(cfg.num_layers)
+        ])
+        self.embed = nn.Sequential(
+            nn.Conv2d(3, cfg.hidden_dim, cfg.patch_size, stride=cfg.patch_size),
+            nn.BatchNorm2d(cfg.hidden_dim),
+            nn.GELU(),
+        )
+        self.digup = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(),
+            nn.Linear(cfg.hidden_dim, cfg.num_classes)
+        )
+
+    def forward(self, x):
+        x = self.embed(x)
+        for layer in self.layers:
+            x = x + layer(x)
+        x = self.digup(x)
+        return x
+
+
+##  layer 1 ###############################################
+
 class Conformer(nn.Module):
     def __init__(self,
                  cfg:ConvMixerCfg):
@@ -143,45 +195,8 @@ class Conformer(nn.Module):
         latent = reduce(latent, 'b n d -> b d', 'mean')
         return self.to_logits(latent)
 
-class Mlp(nn.Module):
-    def __init__(self, dim, hidden_dim=None, dropout = 0.):
-        super().__init__()
-        hidden_dim = dim if hidden_dim is None else hidden_dim
-        self.net = nn.Sequential(
-            nn.Linear(dim, hidden_dim),
-            nn.GELU(),
-            nn.Dropout(dropout),
-            nn.Linear(hidden_dim, dim),
-            nn.Dropout(dropout)
-        )
-    def forward(self, x):
-        return self.net(x)
 
-
-class ConvLayer2(nn.Sequential):
-    def __init__(self, hidden_dim, kernel_size, bias=False):
-        super().__init__(
-            nn.Conv2d(hidden_dim, hidden_dim, 1),
-            nn.BatchNorm2d(hidden_dim),
-            nn.GELU(),
-            nn.Conv2d(hidden_dim, hidden_dim, kernel_size, padding=kernel_size // 2, bias=bias),
-            nn.BatchNorm2d(hidden_dim),
-            nn.GELU(),  
-        ) 
-class ConvLayer3(nn.Sequential):
-    def __init__(self, hidden_dim, kernel_size, bias=False):
-        super().__init__(
-            Residual(nn.Sequential(nn.Conv2d(hidden_dim, hidden_dim, 1),
-            nn.BatchNorm2d(hidden_dim),
-            nn.GELU(),
-            nn.Conv2d(hidden_dim, hidden_dim, kernel_size, padding=kernel_size // 2, bias=bias),
-            nn.BatchNorm2d(hidden_dim),
-            nn.GELU(),
-            )),
-            nn.Conv2d(hidden_dim, hidden_dim, 1),
-            nn.BatchNorm2d(hidden_dim),
-            nn.GELU(),
-        )     
+### layer 2######################################################
 
 class Conformer2(Conformer):
     def __init__(self, cfg: ConvMixerCfg):
@@ -193,22 +208,30 @@ class Conformer2(Conformer):
         )
 
         self.layers = nn.ModuleList([
-            ConvLayer2(cfg.hidden_dim, cfg.kernel_size)
+            ConvLayer(cfg.hidden_dim, cfg.kernel_size)
             for _ in range(cfg.num_layers)
         ])
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(cfg.hidden_dim, cfg.hidden_dim, 1),
-            nn.BatchNorm2d(cfg.hidden_dim),
-            nn.GELU(),
-        )
-        self.mlp = Mlp(cfg.hidden_dim, cfg.hidden_dim * 4)
+        # self.conv1 = nn.Sequential(
+        #     nn.Conv2d(cfg.hidden_dim, cfg.hidden_dim, 1),
+        #     nn.BatchNorm2d(cfg.hidden_dim),
+        #     nn.GELU(),
+        # )
 
-        self.attn_layers = AttnLayer(query_dim=cfg.hidden_dim,
-                                    context_dim=cfg.hidden_dim,
-                                    heads=1,
-                                    dim_head=cfg.hidden_dim,
-                                    )
-        self.norm = nn.LayerNorm(cfg.hidden_dim)
+        self.attn_mlp = nn.ModuleList([])
+        self.attn_mlp.append(PreNorm(cfg.hidden_dim, AttnLayer(query_dim=cfg.hidden_dim,
+                                                               context_dim=cfg.hidden_dim,
+                                                               heads=1,
+                                                               dim_head=cfg.hidden_dim,)))
+        self.attn_mlp.append(PreNorm(cfg.hidden_dim, Mlp(cfg.hidden_dim, cfg.hidden_dim * 4)))
+
+        # self.mlp = Mlp(cfg.hidden_dim, cfg.hidden_dim * 4)
+
+        # self.attn_layers = AttnLayer(query_dim=cfg.hidden_dim,
+        #                             context_dim=cfg.hidden_dim,
+        #                             heads=1,
+        #                             dim_head=cfg.hidden_dim,
+        #                             )
+        # self.norm = nn.LayerNorm(cfg.hidden_dim)
         self.to_logits = nn.Linear(cfg.hidden_dim, cfg.num_classes)
         self.latent = nn.Parameter(torch.randn(1, cfg.hidden_dim))
 
@@ -217,22 +240,27 @@ class Conformer2(Conformer):
         latent = repeat(self.latent, 'n d -> b n d', b=batch_size)
 
         x = self.embed(x)
-        y = self.conv1(x)
-        input = y.permute(0, 2, 3, 1)
+        input = x.permute(0, 2, 3, 1)
         input = rearrange(input, 'b ... d -> b (...) d')
         latent = torch.cat([latent[:, 0][:, None, :], input], dim=1)
-        latent = latent + self.attn_layers(latent, input)
-        mlp = self.mlp(latent) 
+        for attn, mlp in self.attn_mlp:
+            latent = latent + attn(latent, input)
+            mlp = mlp(latent)
+        # latent = latent + self.attn_layers(latent, input)
+        # mlp = self.mlp(latent) 
 
         for layer in self.layers:
             x = x + layer(x)
-            y = self.conv1(x)
-            input = y.permute(0, 2, 3, 1)
+            input = x.permute(0, 2, 3, 1)
             input = rearrange(input, 'b ... d -> b (...) d')
-            # latent = torch.cat([latent[:, 0][:, None, :], input], dim=1)
-            latent = latent + self.attn_layers(latent, input)
-            latent = self.norm(latent)
-            mlp = self.mlp(latent) + mlp
+            latent = torch.cat([latent[:, 0][:, None, :], input], dim=1)
+            # latent = latent + self.attn_layers(latent, input)
+            # latent = self.norm(latent)
+            # mlp = self.mlp(latent) + mlp
+            # mlp = self.norm(mlp)
+            for attn, mlp in self.attn_mlp:
+                latent = latent + attn(latent, input)
+                mlp = mlp + mlp(latent)
 
         latent = reduce(mlp, 'b n d -> b d', 'mean')
         return self.to_logits(latent)

88830b92c539e09dbd188ef0dc72bed1a3f3664e
SGuoying <826367045@qq.com>
2023-06-16 11:22:47 +0800

    update

--- sunyata/mosaicml/model.py
+++ sunyata/mosaicml/model.py
@@ -5,6 +5,7 @@ from composer.metrics import CrossEntropy
 from composer.models import ComposerClassifier
 from torchmetrics import MetricCollection
 from torchmetrics.classification import MulticlassAccuracy
+from sunyata.pytorch.arch.conformer import Conformer
 
 from sunyata.pytorch.arch.convmixer import BayesConvMixer3, BayesConvMixer4, BayesConvMixer5, ConvMixer2, ConvMixerCat, ConvMixerCfg, ConvMixer, BayesConvMixer, ConvMixereca
 from sunyata.pytorch.arch.iterative import ConvMixerattn2, ConvMixerattn3
@@ -43,8 +44,8 @@ def build_composer_convmixer(model_name: str = 'convmixer',
         model = BayesConvMixer4(cfg)
     elif model_name == "bayes_convmixer5":
         model = BayesConvMixer5(cfg)
-    elif model_name == "convmixer_attn_2":
-        model = ConvMixerattn2(cfg)
+    elif model_name == "Conformer":
+        model = Conformer(cfg)
 
     else:
         raise ValueError(f"model_name='{model_name}' but only 'convmixer' and 'bayes_convmixer' are supported now.")
---
+++ sunyata/pytorch/arch/conformer.py
@@ -0,0 +1,123 @@
+from dataclasses import dataclass
+from einops import rearrange, reduce, repeat
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from sunyata.pytorch.arch.base import BaseCfg
+
+
+
+@dataclass
+class ConvMixerCfg(BaseCfg):
+    num_layers: int = 8
+    hidden_dim: int = 256
+    kernel_size: int = 5
+    patch_size: int = 2
+    num_classes: int = 10
+
+    drop_rate: float = 0.
+    squeeze_factor: int = 4
+
+    layer_norm_zero_init: bool = True
+    skip_connection: bool = True
+    eca_kernel_size: int = 3
+
+
+
+class ConvLayer(nn.Sequential):
+    def __init__(self, hidden_dim, kernel_size, bias=False):
+        super().__init__(
+            nn.Conv2d(hidden_dim, hidden_dim, 1),
+            nn.BatchNorm2d(hidden_dim),
+            nn.GELU(),
+            nn.Conv2d(hidden_dim, hidden_dim, kernel_size, padding=kernel_size // 2, bias=bias),
+            nn.BatchNorm2d(hidden_dim),
+            nn.GELU(),
+            nn.Conv2d(hidden_dim, hidden_dim, 1),
+            nn.BatchNorm2d(hidden_dim),
+            nn.GELU(),
+        )
+
+class AttnLayer(nn.Module):
+    def __init__(self, 
+                 query_dim, context_dim=None,
+                 heads=8, dim_head=64, dropout=0.):
+        super().__init__()
+        context_dim = context_dim or query_dim
+        inner_dim = dim_head * heads
+        project_out = not (inner_dim == query_dim and heads == 1)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+        self.dropout = nn.Dropout(dropout)
+        self.to_q = nn.Linear(query_dim, inner_dim, bias=False)
+        self.to_kv = nn.Linear(context_dim, inner_dim * 2, bias=False)
+        self.to_out = nn.Linear(inner_dim, query_dim) if project_out else nn.Identity()
+    
+    def forward(self, x, context=None):
+        h = self.heads
+        q = self.to_q(x)
+        context = context if context is not None else x
+        k, v = self.to_kv(context).chunk(2, dim=-1)
+
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> (b h) n d', h=h), (q,k,v))
+        sim = torch.einsum('b i d, b j d -> b i j', q, k) * self.scale
+        attn = sim.softmax(dim=-1)
+        attn = self.dropout(attn)
+        out = torch.einsum('b i j, b j d -> b i d', attn, v)
+        out = rearrange(out, 'b (h n) d -> b n (h d)', h=h)
+        return self.to_out(out)
+    
+
+class Conformer(nn.Module):
+    def __init__(self,
+                 cfg:ConvMixerCfg):
+        super().__init__()
+        self.cfg = cfg
+        self.layers = nn.ModuleList([
+            ConvLayer(cfg.hidden_dim, cfg.kernel_size)
+            for _ in range(cfg.num_layers)
+        ])
+
+        self.attn_layers = AttnLayer(query_dim=cfg.hidden_dim,
+                                     context_dim=cfg.hidden_dim,
+                                     heads=1,
+                                     dim_head=cfg.hidden_dim,
+                                     dropout=cfg.drop_rate)
+        self.embed = nn.Sequential(
+            nn.Conv2d(3, cfg.hidden_dim, cfg.patch_size, stride=cfg.patch_size),
+            nn.BatchNorm2d(cfg.hidden_dim),
+            nn.GELU(),
+        )
+        self.norm = nn.LayerNorm(cfg.hidden_dim)
+        self.to_logits = nn.Linear(cfg.hidden_dim, cfg.num_classes)
+        self.latent = nn.Parameter(torch.randn(1, cfg.hidden_dim))
+
+    def forward(self, x):
+        b, _, _, _ = x.shape
+        latent = repeat(self.latent, 'n d -> b n d', b=b)
+
+        x = self.embed(x)
+        input = x.permute(0, 2, 3, 1)
+        input = rearrange(input, 'b ... d -> b (...) d')
+        latent = torch.cat([latent[:, 0][:, None, :], input], dim=1)
+        latent = latent + self.attn_layers(latent, input)
+        # latent = rearrange(latent[:, 1:], 'b (h w) d -> b d h w', h=x.shape[2])
+        latent = self.norm(latent)
+
+        for layer in self.layers:
+            x = x + layer(x)
+            input = x.permute(0, 2, 3, 1)
+            input = rearrange(input, 'b ... d -> b (...) d')
+            latent = latent + self.attn_layers(latent, input)
+            latent = self.norm(latent)
+
+        latent = reduce(latent, 'b n d -> b d', 'mean')
+        return self.to_logits(latent)
+           
+        
+
+
+        
+        

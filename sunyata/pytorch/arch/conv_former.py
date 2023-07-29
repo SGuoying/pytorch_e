229f9956a1c46d4ce14cd09d051822bfd44bde25
SGuoying <826367045@qq.com>
2023-07-29 17:50:35 +0800

    update

--- sunyata/pytorch/arch/conv_former.py
+++ sunyata/pytorch/arch/conv_former.py
@@ -551,14 +551,14 @@ class ConvMixerV2_1(nn.Module):
             self.attn.append(attn)
             if i != 2:
                 stage = nn.Sequential(
-                    *[block(hidden_dim=self.hidden_dim, drop_rate=cfg.drop_rate) for _ in range(self.depth[i])]
+                    *[block2(hidden_dim=self.hidden_dim, drop_rate=cfg.drop_rate) for _ in range(self.depth[i])]
                 )
                 self.conv.append(stage)
             else:
                 stage = nn.ModuleList()
                 for j in range(self.depth[i] // self.depth[0]):
                     conv = nn.Sequential(
-                        *[block(hidden_dim=self.hidden_dim, drop_rate=cfg.drop_rate) for _ in range(self.depth[i] //3)]
+                        *[block2(hidden_dim=self.hidden_dim, drop_rate=cfg.drop_rate) for _ in range(self.depth[i] //3)]
                     )
                     stage.append(conv)
                 self.conv.append(stage)
@@ -630,7 +630,7 @@ class ConvMixerV2_2(nn.Module):
 
             conv1 = nn.ModuleList([])
             for _ in range(self.depth[i]):
-                conv2 = block(hidden_dim=self.hidden_dim, drop_rate=cfg.drop_rate)
+                conv2 = block2(hidden_dim=self.hidden_dim, drop_rate=cfg.drop_rate)
                 conv1.append(conv2)
             self.conv.append(conv1)
 
